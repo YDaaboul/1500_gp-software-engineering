5ebed08eab88d72b5baa6afb6c6827061a5b3577
GarismoHussien <hand2land@gmail.com>
2024-11-17 09:08:36 +0200

    T

--- abdelrahman/training/Training1.js
+++ abdelrahman/training/Training1.js
@@ -1,21 +1,29 @@
-let name = 'Ahmed';
-console.log("Hello, " + name);
+// let name = 'Ahmed';
+// console.log("Hello, " + name);
 
-let text = "Handspace ";
-console.log("The length of the text is: " + text.length);
-
-
-let age = 25;
-let Age = 30;
-let AGE = 40;
-console.log("Your age is: " + AGE);
-
-let text = "Handspace Academy";
-let result = text.substring(5, 2); // Incorrect usage of substring
-console.log("Result: " + result);
+// let text = "Handspace ";
+// console.log("The length of the text is: " + text.length);
 
 
+// let age = 25;
+// let Age = 30;
+// let AGE = 40;
+// console.log("Your age is: " + AGE);
 
-let greeting = "Hello, World!";
-let replacedText = greeting.replace("world", "Ahmed"); 
-console.log(replacedText);
+// let text = "Handspace Academy";
+// let result = text.substring(5, 2); // Incorrect usage of substring
+// console.log("Result: " + result);
+
+
+
+// let greeting = "Hello, World!";
+// let replacedText = greeting.replace("World", "Ahmed"); 
+// console.log(replacedText);
+
+
+
+let name = "Ahmed";
+let upperName = name.toUppercase();
+console.log(upperName);
+
+
