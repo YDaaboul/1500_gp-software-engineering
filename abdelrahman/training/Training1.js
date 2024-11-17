63e7e5379efc542e66441082f4af803ff5d2adad
GarismoHussien <hand2land@gmail.com>
2024-11-17 09:04:29 +0200

    T

--- abdelrahman/training/Training1.js
+++ abdelrahman/training/Training1.js
@@ -7,5 +7,12 @@ console.log("The length of the text is: " + text.length);
 
 let age = 25;
 let Age = 30;
+let AGE = 40;
 console.log("Your age is: " + AGE);
+
+let text = "Handspace Academy";
+let result = text.substring(5, 2); // Incorrect usage of substring
+console.log("Result: " + result);
+
+
 
