2c5edc300f145c6f2cfd5704958f77b07710d474
GarismoHussien <hand2land@gmail.com>
2024-11-17 09:00:59 +0200

    :T:

--- abdelrahman/training/Training1.js
+++ abdelrahman/training/Training1.js
@@ -1,8 +1,11 @@
 let name = 'Ahmed';
 console.log("Hello, " + name);
 
-let text = "Handspace";
-console.log("The length of the text is: " + text.length());
+let text = "Handspace ";
+console.log("The length of the text is: " + text.length);
 
 
+let age = 25;
+let Age = 30;
+console.log("Your age is: " + AGE);
 
